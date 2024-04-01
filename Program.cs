daac581d7312531e31d0d3ad156904a70795fb06
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-04-01 20:31:00 -0300

    resolucaoDesafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("123","Tijolão", "321", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Tinder");
+
+Console.WriteLine("Iphone");
+Smartphone iPhone = new Iphone("456","5S", "888", 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Tinder");
